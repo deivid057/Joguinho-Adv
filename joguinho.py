b4fffe743f558684192821ad0e648bf24914fab3
Deivid_Ajs <76882975+deivid057@users.noreply.github.com>
2021-11-18 03:26:53 -0300

    Add files via upload

---
+++ joguinho.py
@@ -0,0 +1,32 @@
+from random import randint
+from time import sleep
+
+while True:
+    print("\n")
+    pc = randint(0, 5)
+    print("-=-" * 20)
+    print("Escolhi um número entre 0 e 5, tente adivinhar! kkk")
+    print("-=-" * 20)
+    print("Exit = 6")
+    print("\n")
+
+    esc = int(input("Qual número você acha que eu escolhi: "))
+
+    if esc == pc:
+        print("PROCESSANDO...")
+        sleep(2)
+        print("PARABÉNS!")
+        print("Você Acertou.(: ")
+        print("-=-" * 20)
+
+    elif esc >= 6:
+        print("Saindo!..................................")
+        sleep(2)
+        break
+
+    else:
+        print("PROCESSANDO...")
+        sleep(2)
+        print("Você ERROu!")
+        print("Eu escolhi {} e não {}".format(pc, esc))
+        print("-=-" * 20)
